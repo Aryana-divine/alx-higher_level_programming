76f528b6e763cd81e585c97e29b4a9881be8cb5f
Aryana-divine <divinearyana0@gmail.com>
2022-08-23 15:42:24 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,6 @@ def uppercase(str):
         if ord(i) >= 97 and ord(i) <= 122:
             strd.append(chr(ord(i) - 32))
         else:
-        strd.append(i)
+            strd.append(i)
             print('{}'.format("".join(strd)), end='')
             print()
