e36067be222a98cc285a632186e2b6a5d7ee902b
Aryana-divine <divinearyana0@gmail.com>
2022-08-25 14:35:23 -0700

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-funcs = dir(hidden_4)
-
 if __name__ == "__main__":
-    for f in funcs:
-        if '__' not in f:
-            print(f)
+    import sys
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+        print("{}".format(result))
