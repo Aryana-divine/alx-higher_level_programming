8edaa5988caace7adc180ed3592a1ff7471c8264
Aryana-divine <divinearyana0@gmail.com>
2022-08-29 07:57:03 -0700

    Header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,14 +21,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-listint_t *insert_node(listint_t **head, int number);
+
 int is_palindrome(listint_t **head);
-def element_at(my_list, idx);
-def replace_in_list(my_list, idx, element);
-def print_reversed_list_integer(my_list=[]);
-def new_in_list(my_list, idx, element);
-def no_c(my_string);
-def print_matrix_integer(matrix=[[]]);
 
-
-#endif
+#endif /* LISTS_H */
